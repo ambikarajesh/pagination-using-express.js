1b80e2787e9f6a6d0810a62095fa276df5adb069
ambikarajesh <ambikula@gmail.com>
2019-03-29 09:25:33 -0700

    pagination done without pakage

--- controllers/image.js
+++ controllers/image.js
@@ -1,11 +1,21 @@
 const Image = require('../models/image');
-exports.getImages = async(req, res, next) =>{    
-    Image.find().then(images => {
-    res.render('image-list', {
-        title:'Gallery',
-        images: images
-    }) 
-}).catch(err=> console.log(err))     
+const PAGE_PER_IMAGES = 2;
+exports.getImages = async(req, res, next) =>{   
+    const currentPage = +req.query.page; 
+    let totalImages;
+    Image.find().count().then(total => {
+        totalImages = total;
+        return Image.find().skip((currentPage-1)*PAGE_PER_IMAGES).limit(PAGE_PER_IMAGES)
+    }).then(images => {
+        res.render('image-list', {
+            title:'Gallery',
+            images: images,
+            currentPage:currentPage,
+            pages:Math.ceil(totalImages/PAGE_PER_IMAGES),
+            hasPrevPage:currentPage > 1,
+            hasNextPage:Math.ceil(totalImages/PAGE_PER_IMAGES) > currentPage
+        }) 
+    }).catch(err=> console.log(err))     
 }
 
 exports.getImage = (req, res, next) =>{
